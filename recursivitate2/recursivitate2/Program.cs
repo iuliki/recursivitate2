c0a71a779bdf97e2e5741c1a93e0177a9d404fb5
iuliki <iulia.bulugu@emanuel.ro>
2022-01-13 15:58:43 +0200

    rezolvare

---
+++ recursivitate2/recursivitate2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace recursivitate2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] array = new int [] { 1, 34, 3, 78, 5, 6, 7, 8 };
+
+            Console.WriteLine(maxim.Maximul(array, array.Length-1 ,int.MinValue));
+        }
+    }
+}
---
+++ recursivitate2/recursivitate2/maxim.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace recursivitate2
+{
+    class maxim
+    {
+        public static int Maximul(int[] array, int n, int maxi)
+        {
+            if(n==0 )
+            {
+                if (maxi >= array[n])
+                    return maxi;
+                else
+                    return array[n];
+            }
+            else
+            {
+                if (maxi >= array[n])
+                    return Maximul(array,n-1,maxi);
+                else
+                    return Maximul(array, n - 1, array[n]);
+            }
+        }
+    }
+}
